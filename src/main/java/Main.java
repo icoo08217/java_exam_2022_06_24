55938337ee3f086d00da0644e2d162364b029537
icoo08217 <icoo08216@gmail.com>
2022-06-24 15:42:45 +0900

    작업1

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        car1.maxSpeed = 230;
+        car2.maxSpeed = 210;
+
+        car1.달리다();
+        car2.달리다();
+    }
+}
+
+class Car{
+    int maxSpeed;
+
+    public void 달리다(){
+        System.out.println("자동차가 최고속력 " + this.maxSpeed + "km로 달립니다.");
+    }
+}
